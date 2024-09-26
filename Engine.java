b0a5f6c83bb34200221e47f5a8fcd300426d65b8
Ada <ajli@smith.edu>
2024-09-26 11:54:46 -0400

    Added lines of code to test the function.

--- Engine.java
+++ Engine.java
@@ -8,6 +8,7 @@ public class Engine {
    public Engine(FuelType f, double maxFuelLevel){
     this.f = f;
     this.maxFuelLevel = maxFuelLevel; 
+    this.currentFuelLevel = maxFuelLevel;
    }
 
    //Max fuel level accessor
@@ -28,22 +29,24 @@ public class Engine {
    }
    //Go function 
    public boolean go(){
+    this.currentFuelLevel -= 1.0;
+    System.out.println(currentFuelLevel);
     if(currentFuelLevel>0.0){
-        System.out.println(currentFuelLevel);
-        this.currentFuelLevel -= 1.0;
         return true;
     }else{
         return false;
     }
-
+    
    }
 
    //main function
    public static void main(String[] args) {
     Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
-    myEngine.getmaxFuelLevel();
+    System.out.println(myEngine.getmaxFuelLevel());
+    myEngine.go();
+    System.out.println(myEngine.getcurrentFuelLevel());
     while (myEngine.go()) {
-        System.out.println("Choo choo!");
+       System.out.println("Choo choo!");
     }
     System.out.println("Out of fuel.");
    }
