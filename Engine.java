7fe7b48e5958a3e49e728bdf317f9afdc186081a
Ada <ajli@smith.edu>
2024-09-26 11:18:06 -0400

    Added space to Passenger to start adding methods, inserted the myEngine.go() to test if it worked, and commented out example statement

--- Engine.java
+++ Engine.java
@@ -41,9 +41,10 @@ public class Engine {
    //main function
    public static void main(String[] args) {
     Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
-    while (myEngine.go()) {
-        System.out.println("Choo choo!");
-    }
-    System.out.println("Out of fuel.");
+    myEngine.go();
+    // while (myEngine.go()) {
+    //     System.out.println("Choo choo!");
+    // }
+    // System.out.println("Out of fuel.");
    }
 }
--- Passenger.java
+++ Passenger.java
@@ -5,4 +5,5 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+    
 }
