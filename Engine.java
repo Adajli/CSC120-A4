ea6f4b9eef6e5533a80bc6a0d606668952cebbaf
Ada <ajli@smith.edu>
2024-09-26 11:13:59 -0400

    Created the refuel method, the accessors, the go methods, the constructor, and the attributes needed for Engine class

--- Engine.java
+++ Engine.java
@@ -1,3 +1,46 @@
 public class Engine {
+   //Attributes
+   FuelType f;
+   private double currentFuelLevel;
+   private double maxFuelLevel;
 
+   //Constructor
+   public Engine(FuelType fuelType, double maxFuelLevel){
+    f = fuelType;
+    this.maxFuelLevel = maxFuelLevel; 
+   }
+   
+   //Max fuel level accessor
+   public double getmaxFuelLevel(){
+    return maxFuelLevel;
+   }
+   //Current fuel level accessor
+   public double getcurentFuelLevel(){
+    return currentFuelLevel;
+   }
+   //Fuel type accessor
+   // Refuel method to refuel engine
+   public void refuel(){
+    currentFuelLevel = maxFuelLevel;
+   }
+   //Go function 
+   public boolean go(){
+    if(currentFuelLevel>0.0){
+        System.out.println(currentFuelLevel);
+        currentFuelLevel -= 1.0;
+        return true;
+    }else{
+        return false;
+    }
+
+   }
+
+   //main function
+   public static void main(String[] args) {
+    Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+    while (myEngine.go()) {
+        System.out.println("Choo choo!");
+    }
+    System.out.println("Out of fuel.");
+   }
 }
