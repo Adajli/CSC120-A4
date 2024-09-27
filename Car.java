786534b7aa710e10e889df65cae08809ad0c2041
Ada <ajli@smith.edu>
2024-09-26 20:49:57 -0400

    Added the attributes, constructors, and accessor like methods with comments.

--- Car.java
+++ Car.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
 public class Car {
+    //Attributes of car
+    private ArrayList<Passenger> passengersOnboard;
+    private int maxCapacity;
 
+    //Constructor
+    public Car(ArrayList<Passenger> passengersOnboard, int maxCapacity){
+        this.passengersOnboard = passengersOnboard;
+        this.maxCapacity = maxCapacity;
+    }
+    //Method returns maximum capacity of the car
+    public int getCapacity(){
+        return this.maxCapacity;
+    }
+    //Method returns seats remaining in the car
+    public int seatsRemaining(){
+        return maxCapacity-passengersOnboard.size();
+    }
 }
