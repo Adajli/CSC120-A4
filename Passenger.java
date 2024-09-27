b41c4183460808e60932200a6dca2b354cf05b3b
Ada <ajli@smith.edu>
2024-09-27 18:36:39 -0400

    Added comment to new method getPassenger and adjusted statements in the if else brackets.

--- Passenger.java
+++ Passenger.java
@@ -5,19 +5,23 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+    //Method gets information about passenger.
+    public String getPassenger(){
+        return this.name;
+    }
     //boardCar function
     public void boardCar(Car c){
         if(c.addPassenger(this)){
-            System.out.println("The passenrger already boarded");
+            System.out.println(getPassenger()+ "already boarded.");
         }else{
-            System.out.println("The car is already full");
+            System.out.println("The car is already full.");
         }
     }
     public void getoff(Car c){
         if(c.addPassenger(this)){
-            System.out.println("The passenger is already removed");
+            System.out.println( getPassenger()+ "is off the train.");
         }else{
-            System.out.println("The pasenger is not on board");
+            System.out.println( getPassenger() +"is not on board.");
         }
     }
     
