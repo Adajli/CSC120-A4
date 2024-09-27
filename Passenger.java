7a14fc3c841f57fc17ec75ebbe3eaf279ab4ede3
Ada <ajli@smith.edu>
2024-09-27 17:11:06 -0400

    Added getOff() method to Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -14,6 +14,14 @@ public class Passenger {
             System.out.println("The car is already full");
         }
     }
-
+    public void getoff(Car c){
+        c.addPassenger(this);
+        if(c.addPassenger(this)){
+            System.out.println("The passenrger" + name +" already boarded");
+        }else{
+            System.out.println("The car is already full");
+        }
+    }
+    
 
 }
