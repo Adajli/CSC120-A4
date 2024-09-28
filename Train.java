f653c73982fec5df3ff72928d95c7e87438e5032
Ada <ajli@smith.edu>
2024-09-27 20:52:59 -0400

    Added attributes and part of the constructor in Train

--- Train.java
+++ Train.java
@@ -1,3 +1,19 @@
 public class Train {
+    //Attributes
+    private FuelType fuelType;
+    private double fuelCapacity;
+    private int nCars;
+    private int passengerCapacity;
+
+    //Train constructor
+    public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity){
+        this.fuelType = fuelType;
+        this.fuelCapacity = fuelCapacity;
+        this.nCars = nCars;
+        this.passengerCapacity = passengerCapacity;
+        Engine e = new Engine(fuelType, fuelCapacity);
+        Car c = new Car(, passengerCapacity);
+    }
+
 
 }
