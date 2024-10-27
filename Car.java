327d53234ac05b0e94b1a7b262e1bd4f5ec2aa3a
Adajli <ajli@smith.edu>
2024-10-27 14:08:30 -0400

    Statement modified from printing to throwing a runtime exception and removed unnecessary code.

--- Car.java
+++ Car.java
@@ -1,80 +1,98 @@
 import java.util.ArrayList;
+
 public class Car {
-    //Attributes of car
+    // Attributes of car
     private ArrayList<Passenger> passengersOnboard;
     private int maxCapacity;
 
-    /**Constructor
+    /**
+     * Constructor
+     * 
      * @param maxCapcity
-    */
-    public Car(int maxCapacity){
+     */
+    public Car(int maxCapacity) {
         this.passengersOnboard = new ArrayList<Passenger>(maxCapacity);
         this.maxCapacity = maxCapacity;
     }
-    /** Method acesses maximum capacity of the car
+
+    /**
+     * Method acesses maximum capacity of the car
+     * 
      * @return integer maximum capacity
      */
-    public int getCapacity(){
+    public int getCapacity() {
         return this.maxCapacity;
     }
 
-     /**Method acesses array of passengers onboard
-      * @return ArrayList<Passenger>
-      */
-     public ArrayList<Passenger>  getPassengersOnboard(){
+    /**
+     * Method acesses array of passengers onboard
+     * 
+     * @return ArrayList<Passenger>
+     */
+    public ArrayList<Passenger> getPassengersOnboard() {
         return this.passengersOnboard;
     }
-    /** Method returns seats remaining in the car
+
+    /**
+     * Method returns seats remaining in the car
+     * 
      * @return seats left
-    */
-    public int seatsRemaining(){
-        return maxCapacity-passengersOnboard.size();
+     */
+    public int seatsRemaining() {
+        return maxCapacity - passengersOnboard.size();
     }
 
-    /** Method to add a passenger
+    /**
+     * Method to add a passenger
+     * 
      * @param passenger
      * @return boolean
      */
-    public boolean addPassenger(Passenger p){
-        if(passengersOnboard.size()<maxCapacity && !p.equals(null) || !p.equals(p)){
+    public boolean addPassenger(Passenger p) {
+        if (passengersOnboard.size() < maxCapacity && !p.equals(null) || !p.equals(p)) {
             passengersOnboard.add(p);
             return true;
-        }else{
-           throw new RuntimeException("Can't add the passenger to the car of the train.");
+        } else {
+            throw new RuntimeException("Can't add the passenger to the car of the train.");
         }
     }
 
-    /** Method to remove the passenger 
-     *  @param passenger
-     *  @return boolean
-    */
-    public boolean removePassenger(Passenger p){
+    /**
+     * Method to remove the passenger
+     * 
+     * @param passenger
+     * @return boolean
+     */
+    public boolean removePassenger(Passenger p) {
         boolean removed = false;
-        if(passengersOnboard.contains(p)){
-            for(int i = 0; i<passengersOnboard.size(); i++){
-                if(passengersOnboard.get(i).equals(p)){
+        if (passengersOnboard.contains(p)) {
+            for (int i = 0; i < passengersOnboard.size(); i++) {
+                if (passengersOnboard.get(i).equals(p)) {
                     passengersOnboard.remove(p);
                     removed = true;
                 }
             }
-        }else{
-            System.out.println("Can't remove passenger from train.");
+        } else {
+            throw new RuntimeException("Can't remove passenger from train.");
         }
         return removed;
     }
-    /** Manifest function prints out information 
+
+    /**
+     * Manifest function prints out information
+     * 
      * @return void
-    */
-    public void printManifest(){
-        if(passengersOnboard.size()==0){
+     */
+    public void printManifest() {
+        if (passengersOnboard.size() == 0) {
             System.out.println("This car is empty.");
-        }else{
-            for(int i = 0; i<passengersOnboard.size(); i++){
-                System.out.println(passengersOnboard.get(i).getPassenger());           
+        } else {
+            for (int i = 0; i < passengersOnboard.size(); i++) {
+                System.out.println(passengersOnboard.get(i).getPassenger());
             }
         }
     }
-    
+
     // Main function used for testing
     public static void main(String[] args) {
         Car c = new Car(10);
