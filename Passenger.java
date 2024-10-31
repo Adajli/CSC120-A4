50eeae00d654a08691a266f4e44e29def5eeb712
Adajli <ajli@smith.edu>
2024-10-31 09:34:34 -0400

    Modifications for function in Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,52 +1,60 @@
 /* Passenger function shows actions and attributes of the passenger. */
 public class Passenger {
-    //Attribute
+    // Attribute
     private String name;
+
     /**
      * Constructor for passenger
+     * 
      * @param name
      */
     public Passenger(String name) {
         this.name = name;
     }
-    /** Method gets information about passenger.
+
+    /**
+     * Method acceses information about passenger.
+     * 
      * @return String
-    */
-    public String getPassenger(){
+     */
+    public String getPassenger() {
         return this.name;
     }
-    /** boardCar function allows passenger to board car
+
+    /**
+     * boardCar function allows passenger to board car
+     * 
      * @param c
      * @retuurn void
-    */
-    public void boardCar(Car c){
+     */
+    public void boardCar(Car c) {
         try {
-            c.addPassenger(this);
-            if(c.addPassenger(this)){
-                System.out.println(getPassenger()+ " already boarded.");
+            if (c.addPassenger(this)) {
+                System.out.println(getPassenger() + " already boarded.");
             }
         } catch (Exception e) {
             System.out.println("The car is already full.");
         }
-       
+
     }
-    /** Function lets passenger get off the car.
+
+    /**
+     * Function lets passenger get off the car.
+     * 
      * @param c
      * @return void
-    */
-    public void getOffCar(Car c){
+     */
+    public void getOffCar(Car c) {
         try {
-            c.addPassenger(this);
-            if(c.removePassenger(this)){
-                System.out.println( getPassenger()+ " is off the train.");
+            if (c.removePassenger(this)) {
+                System.out.println(getPassenger() + " is off the train.");
             }
         } catch (Exception e) {
-            System.out.println( getPassenger() +" is not on board.");
-        } 
+            System.out.println(getPassenger() + " is not on board.");
+        }
     }
-    
-    
-    /*Main function used for testing*/
+
+    /* Main function used for testing */
     public static void main(String[] args) {
         Passenger p = new Passenger("Melissa");
         System.out.println(p.getPassenger());
